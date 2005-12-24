e604f0a230373d040c08c53aeed1f4f6e38e7450
Arkadiusz Miśkiewicz <arekm@maven.pl>
2005-12-24 22:00:55 +0000

    - wxversion.py which always returns that everything is ok
    Changed files:
        python-wxPython-wxversion-null.py -> 1.1

---
+++ python-wxPython-wxversion-null.py
@@ -0,0 +1,9 @@
+def select(versions, optionsRequired=False):
+	pass
+def ensureMinimal(minVersion, optionsRequired=False):
+	pass
+def checkInstalled(versions, optionsRequired=False):
+	return True
+def getInstalled():
+	return "wxPython"
+
