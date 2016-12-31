b97867a15fadca6b7085a79387b927b4aba94701
Arkadiusz Miśkiewicz <arekm@maven.pl>
2016-12-31 12:13:09 +0100

    - rel 4; missing exceptions in our fake wxversion module

--- python-wxPython-wxversion-null.py
+++ python-wxPython-wxversion-null.py
@@ -1,9 +1,18 @@
+class VersionError(Exception):
+	pass
+
+class AlreadyImportedError(VersionError):
+	pass
+
 def select(versions, optionsRequired=False):
 	pass
+
 def ensureMinimal(minVersion, optionsRequired=False):
 	pass
+
 def checkInstalled(versions, optionsRequired=False):
 	return True
+
 def getInstalled():
 	return "wxPython"
 
